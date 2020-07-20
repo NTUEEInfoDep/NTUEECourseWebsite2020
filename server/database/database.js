90696001622ad2d3bbfc719c923aac4a5d80c551
MortalHappiness <b07901069@ntu.edu.tw>
2020-07-20 21:59:42 +0800

    Add command cli script

---
+++ server/database/database.js
@@ -0,0 +1,39 @@
+// Database operations cli
+
+// ========================================
+
+require("yargs") // eslint-disable-line
+  .usage("Usage: node $0 <command>")
+  .command(
+    "reset",
+    "Reset all data in database.",
+    () => {},
+    (argv) => {
+      console.log(argv);
+    }
+  )
+  .command(
+    "export",
+    "Export student selections.",
+    () => {},
+    (argv) => {
+      console.log(argv);
+    }
+  )
+  .command(
+    "update-opentime",
+    "Update openTime.",
+    (yargs) => {
+      yargs.positional("resource", {
+        describe: "Resource to be update",
+        choices: ["opentime"],
+      });
+    },
+    (argv) => {
+      console.log(argv);
+    }
+  )
+  .alias("h", "help")
+  .version(false)
+  .strictCommands(true)
+  .demandCommand(1, "No command specified.").argv;
